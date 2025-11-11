13db68e3d8d9c4088088737eac7cc93cda59a2ec
isula2004 <isula2004@gmail.com>
2025-11-11 14:58:37 +0530

    syntax issue fix

--- server.js
+++ server.js
@@ -32,9 +32,10 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3030;
-http.listen(PORT, "0.0.0.0", () =>
-  console.log(🚀 Server running on port ${PORT})
-);
+http.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
 
 
 
